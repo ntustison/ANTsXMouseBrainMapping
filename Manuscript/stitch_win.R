5ed8992d4e4bb31fae215f0487c1018cf536db99
Min Chen <dontminchenit@gmail.com>
2024-08-27 16:44:07 -0400

    separate R and Windows scripts

---
+++ Manuscript/stitch_win.R
@@ -0,0 +1,37 @@
+library( rmarkdown )
+library( ggplot2 )
+
+stitchedFile <- "stitched.md"
+
+rmdFiles <- c( "format.md",
+               "titlePage.md",
+               "abstract.md",
+               "intro.md",
+               "introAnts.md",
+               "resultsAllen.md",
+               "resultsDevCCF.md",
+               "resultsKK.md",
+               "discussion.md",
+               "methods.md",
+               "data_availability.md",
+               "acknowledgments.md",
+               "author_contributions.md",
+               "references.md"
+             )
+
+for( i in 1:length( rmdFiles ) )
+  {
+  cat( rmdFiles[i] )
+  if( i == 1 )
+    {
+    cmd <- paste( "type", rmdFiles[i], ">", stitchedFile )
+    } else {
+    cmd <- paste( "type", rmdFiles[i], ">>", stitchedFile )
+    }
+  shell( cmd )
+  }
+
+cat( '\n Pandoc rendering', stitchedFile, '\n' )
+render( stitchedFile, pdf_document( number_sections = TRUE, pandoc_args = "--variable=subparagraph" ) )
+render( stitchedFile, latex_document() )
+
